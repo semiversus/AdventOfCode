34ad14ba47532c282231df01f565d69c68f60b10
Günther Jena <guenther@jena.at>
2023-12-04 22:13:45 +0100

    add clean solution 2021/day10

--- template/apply_template.py
+++ template/apply_template.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 import shutil
 
-YEAR = 2023
+YEAR = 2021
 
-for day in range(4, 10):
+for day in range(10, 14):
     target = Path(f'../{YEAR}/day{day:02d}')
     shutil.copytree('../template', target)
     content = (target / 'solution.ipynb').read_text()
