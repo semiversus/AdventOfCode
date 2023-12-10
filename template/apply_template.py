f24ca079fc681fe86bab41a5dbe3090aa0e3c4da
Günther Jena <guenther@jena.at>
2023-12-10 21:28:14 +0100

    solve 2021/day14

--- template/apply_template.py
+++ template/apply_template.py
@@ -1,11 +1,16 @@
 from pathlib import Path
 import shutil
 
-YEAR = 2021
-
-for day in range(10, 14):
-    target = Path(f'../{YEAR}/day{day:02d}')
+def add(year: int, day: int):
+    target = Path(f'../{year}/day{day:02d}')
     shutil.copytree('../template', target)
     content = (target / 'solution.ipynb').read_text()
-    (target / 'solution.ipynb').write_text(content.replace('YYYY', f'{YEAR}').replace('DD', f'{day}'))
+    (target / 'solution.ipynb').write_text(content.replace('YYYY', f'{year}').replace('DD', f'{day}'))
     (target / 'apply_template.py').unlink()
+
+if __name__ == '__main__':
+    from sys import argv
+    if len(argv) != 3:
+        print('need YEAR and DAY as arguments')
+    year, day = int(argv[1]), int(argv[2])
+    add(year, day)
